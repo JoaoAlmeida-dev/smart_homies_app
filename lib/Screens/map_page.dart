72086440e2a65d4d87e8680404c5137158d3c494
joao.ranger@gmail.com <Joao24072000>
2021-07-07 01:31:40 +0100

    google maps setup

--- lib/Screens/map_page.dart
+++ lib/Screens/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:smart_homies_app/Widgets/my_scaffold.dart';
 
 import '../main.dart';
@@ -7,11 +8,22 @@ import '../main.dart';
 class MapPage extends StatelessWidget {
   const MapPage({Key? key}) : super(key: key);
 
+  static const CameraPosition initialPosition =
+  CameraPosition(
+    target: LatLng(200, 200),
+    zoom: 15,
+  );
+  
   @override
   Widget build(BuildContext context) {
     return const MyScaffold(
       body: Center(
-        child: Text('MapPage'),
+        child:
+        GoogleMap(
+          mapType: MapType.hybrid,
+            initialCameraPosition: initialPosition,
+
+        ),
       ),
     );
   }
